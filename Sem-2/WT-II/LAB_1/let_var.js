2ec17c48b9453c5c79a41dfe818d6fa6acd23db4
Ghanshyam <ghanshyampanera80@gmail.com>
2024-11-28 10:29:31 +0530

    let var difference

---
+++ Sem-2/WT-II/LAB_1/let_var.js
@@ -0,0 +1,16 @@
+var a=10;
+let b=20;
+
+function print(){
+    var a=30;
+    let d=40;
+    let c=50
+    console.log(a);
+    console.log(c);
+    console.log(b);
+}
+print();
+var a=60;
+console.log(a);
+// console.log(c);error
+// console.log(d);error
