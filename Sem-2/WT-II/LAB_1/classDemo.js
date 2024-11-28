b0369da4a9b43e2834ce6fa41e847538aaa5bf76
Ghanshyam <ghanshyampanera80@gmail.com>
2024-11-28 11:25:39 +0530

    modified class demo

--- Sem-2/WT-II/LAB_1/classDemo.js
+++ Sem-2/WT-II/LAB_1/classDemo.js
@@ -1,14 +1,18 @@
 class Person{
+    #name;
+    #sem;
+    #collage;//private
+    pub=10;//public
+    _pro=20;//protected
     constructor(name,sem){
-        this.name=name;
-        this.sem=sem;
-        this.collage="DU";
+        this.#name=name;
+        this.#sem=sem;
+        this.#collage="DU";
     }
     display(){
-
-        console.log(this.name);
-        console.log(this.sem);
-        console.log(this.collage);
+        console.log(this.#name);
+        console.log(this.#sem);
+        console.log(this.#collage);
     }
 }
 
@@ -17,4 +21,8 @@ const p2=new Person("xyz",17);
 
 console.log(p1.display());
 console.log(p2.display());
+console.log(p1.name);
+console.log(p1.pub);
+console.log(p1._pro);
+
 
