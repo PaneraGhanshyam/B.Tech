3c38b596f68c7180e9e5b46bdd1a16eb14b8963d
Ghanshyam <ghanshyampanera80@gmail.com>
2024-11-28 10:46:57 +0530

    created destructuring demo

---
+++ Sem-2/WT-II/LAB_1/destructuring.js
@@ -0,0 +1,13 @@
+let a=[10,20,30];
+
+let [n1,n2,n3]=a;
+console.log(n1);
+console.log(n2);
+console.log(n3);
+
+let b={name:"ghanshyam",sem:4};
+let {name,sem}=b;
+
+console.log(name);
+console.log(sem);
+
